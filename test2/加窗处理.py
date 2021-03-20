43162c8c0fb59308d98716458e66e3414e55e328
Rvelamen <13126348842@163.com>
2021-03-21 00:25:36 +0800

    汉明窗

--- test2/加窗处理.py
+++ test2/加窗处理.py
@@ -1,9 +1,33 @@
 # -*- coding:utf-8 -*-
 __author__: 'Rvelamen'
-__data__ = '2021/3/19 15:04'
+__data__ = '2021/3/20 23:44'
 
 from matplotlib import pyplot as plt
 import numpy as np
+
+plt.rcParams['font.sans-serif']=['SimHei']
+plt.rcParams['axes.unicode_minus']=False
+
+def choose_windows(type, wlen):
+    """
+    type
+        1、 汉明窗
+        2、 海宁窗
+        3、 矩形窗
+        4、 三角窗
+    :param type:
+    :param wlen:
+    :return:
+    """
+    if type == 1:
+        window = np.array([0.54 - 0.46 * np.cos(2 * np.pi * n / (wlen - 1)) for n in range(wlen)])
+    elif type == 2:
+        window = np.array([0.5 * (1 - np.cos(2 * np.pi * n / (wlen - 1))) for n in range(wlen)])
+    elif type == 3:
+        window = np.ones(wlen)
+    elif type == 4:
+        window = signal.triang(wlen)
+    return window
 
 wlen = 64                          # 帧长
 inc = 32                           # 帧移
@@ -22,19 +46,29 @@ indices = np.tile(np.arange(0, wlen), (nf, 1)) + np.tile(np.arange(0, nf * inc, 
 indices = np.array(indices, dtype=np.int32)
 frames = pad_signal[indices]
 a = frames[2:3]
+
+# 加窗前
 plt.figure(figsize=(10, 4))
 plt.subplot(121)
-plt.title("The Original waveform")
-plt.xlabel("time(/s)")
-plt.ylabel("Amplitude")
-plt.legend()
-plt.plot(time, a[0])
-
-plt.subplot(122)
+plt.title("未加窗振幅图")
+plt.xlabel("振幅值")
+plt.ylabel("频率")
 amp = np.arange(wlen)
 x = np.fft.fft(a[0])
 abs_y = np.abs(x)
 plt.plot(amp[range(int(wlen/2))], abs_y[range(int(wlen/2))])
 
-plt.show()
+# 添加汉明窗
+plt.subplot(122)
+plt.title("填加汉明窗振幅图")
+plt.xlabel("振幅值")
+plt.ylabel("频率")
+window = choose_windows(1, wlen)
+b = a[0]*window
+x = np.fft.fft(b)
+abs_y = np.abs(x)
+plt.plot(amp[range(int(wlen/2))], abs_y[range(int(wlen/2))])
 
+
+
+plt.show()
