3fab436c0330cc357b7249e52df4a117d7110929
Rvelamen <13126348842@163.com>
2021-03-20 23:40:04 +0800

    语音信号处理结构、频谱泄露、非周期截断导致拓延信号与原信号不连续

--- test2/频谱泄露.py
+++ test2/频谱泄露.py
@@ -5,15 +5,15 @@ __data__ = '2021/3/19 15:04'
 from matplotlib import pyplot as plt
 import numpy as np
 
-wlen = 128                          # 帧长
-inc = 64                            # 帧移
+wlen = 64                          # 帧长
+inc = 32                           # 帧移
 N = 1000                            # 采样点个数
 t = np.linspace(0, 1, N)            # 得到各个点采样时间(1秒)
 time = np.arange(0, wlen) * (1.0/N)      # 一帧的时间长度
 
 y = np.sin(2 * np.pi * 250 * t) + 2 * np.sin(2 * np.pi * 185.5 * t)  # 函数
 
-nf = int(np.ceil((1.0 * N - wlen + inc) / inc) ) # 帧数
+nf = int(np.ceil((1.0 * N - wlen + inc) / inc)) # 帧数
 pad_length = int((nf - 1) * inc + wlen)     # 加上超出部分
 zeros = np.zeros((pad_length - N,))            # 补零
 pad_signal = np.concatenate((y, zeros))     # 填补后的信号
@@ -36,4 +36,10 @@ x = np.fft.fft(a[0])
 abs_y = np.abs(x)
 plt.plot(amp[range(int(wlen/2))], abs_y[range(int(wlen/2))])
 
+plt.show()
+# 合成第2帧的情况
+print(time)
+print(time+wlen*1.0/N)
+print(np.concatenate((time, time+wlen*1.0/N)))
+plt.plot(np.concatenate((time, time+wlen*1.0/N)), np.concatenate((a[0], a[0])))
 plt.show()
